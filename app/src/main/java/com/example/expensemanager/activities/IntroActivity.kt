8c756053c30ff1490ea20416de434aed05409cc4
Deepanshu Rawat <deepanshu.rawat@zalora.com>
2021-10-03 20:38:27 +0800

    added intro activities

---
+++ app/src/main/java/com/example/expensemanager/activities/IntroActivity.kt
@@ -0,0 +1,30 @@
+package com.example.expensemanager.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import com.example.expensemanager.R
+import kotlinx.android.synthetic.main.activity_intro.*
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+        // This is used to hide the status bar and make the splash screen as a full screen activity.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        btn_sign_up_intro.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+
+        btn_sign_in_intro.setOnClickListener {
+            startActivity(Intent(this, SignInActivity::class.java))
+        }
+
+
+    }
+}
