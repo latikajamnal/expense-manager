bbe18fdc31db0a72fffcde6223b7586e58565b8a
Deepanshu Rawat <deepanshu.rawat@zalora.com>
2021-10-10 14:47:24 +0800

    added logic to skip intro screen, if already logged in

--- app/src/main/java/com/example/expensemanager/activities/MainActivity.kt
+++ app/src/main/java/com/example/expensemanager/activities/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.expensemanager.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.expensemanager.R
 
 class MainActivity : BaseActivity() {
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
