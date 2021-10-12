70e3ef5e660e914c6d5134e37232fe0254c8971b
Deepanshu Rawat <deepanshu.rawat@zalora.com>
2021-10-12 22:42:42 +0800

    added setting activity

--- app/src/main/java/com/example/expensemanager/activities/MainActivity.kt
+++ app/src/main/java/com/example/expensemanager/activities/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.expensemanager.activities
 
+import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.example.expensemanager.R
 
 class MainActivity : BaseActivity() {
     private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
+
---
+++ app/src/main/java/com/example/expensemanager/activities/SettingAcitivity.kt
@@ -0,0 +1,12 @@
+package com.example.expensemanager.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.expensemanager.R
+
+class SettingAcitivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting_acitivity)
+    }
+}
