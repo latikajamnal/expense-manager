9ca969485cd3ee2a4a9b92ce2a8257870a64dcaa
Deepanshu Rawat <deepanshu.rawat@zalora.com>
2021-10-13 21:58:19 +0800

    used fragments instead of separate activities

---
+++ app/src/main/java/com/example/expensemanager/activities/HomeFragment.kt
@@ -0,0 +1,9 @@
+package com.example.expensemanager.activities
+
+import androidx.fragment.app.Fragment
+import com.example.expensemanager.R
+
+
+class HomeFragment : Fragment(R.layout.home) {
+
+}
--- app/src/main/java/com/example/expensemanager/activities/MainActivity.kt
+++ app/src/main/java/com/example/expensemanager/activities/MainActivity.kt
@@ -1,15 +1,8 @@
 package com.example.expensemanager.activities
 
-import android.app.Activity
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.example.expensemanager.HomeFragment
 import com.example.expensemanager.R
-import com.example.expensemanager.SettingFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -17,8 +10,6 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
         val settingFragment = SettingFragment()
         val homeFragment = HomeFragment()
         setCurrentFragment(homeFragment)
