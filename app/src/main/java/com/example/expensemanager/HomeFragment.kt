ef8fa1b36798bfbdec225a049a87c32d41b68e5f
Deepanshu Rawat <deepanshu.rawat@zalora.com>
2021-10-13 21:50:42 +0800

    added fragments

---
+++ app/src/main/java/com/example/expensemanager/HomeFragment.kt
@@ -0,0 +1,12 @@
+package com.example.expensemanager
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class HomeFragment : Fragment(R.layout.activity_main) {
+
+}
--- app/src/main/java/com/example/expensemanager/activities/MainActivity.kt
+++ app/src/main/java/com/example/expensemanager/activities/MainActivity.kt
@@ -2,18 +2,40 @@ package com.example.expensemanager.activities
 
 import android.app.Activity
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.expensemanager.HomeFragment
 import com.example.expensemanager.R
+import com.example.expensemanager.SettingFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     private val TAG = "MainActivity"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        val settingFragment = SettingFragment()
+        val homeFragment = HomeFragment()
+        setCurrentFragment(homeFragment)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_settings -> setCurrentFragment(settingFragment)
+                R.id.menu_home -> setCurrentFragment(homeFragment)
+            }
+            true
+        }
     }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, fragment)
+            commit()
+        }
 }
 
